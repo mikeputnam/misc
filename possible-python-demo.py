198362f4c2785e60b2d7764db25ae283fd87c745
Mike Putnam <mike@theputnams.net>
2013-10-29 10:35:00 -0500

    DHMN web stats

--- possible-python-demo.py
+++ possible-python-demo.py
@@ -13,4 +13,10 @@ p.data = {"legalname":"John Doe","eyecolor":"brown","typeofperson":"worker"}
 
 print "p.__doc__    :", p.__doc__
 print "p.data   :", p.data
-print "p.data['legalname']  :", p.data['legalname']
+print "p.data.get('legalname',None)  :", p.data.get('legalname',None)
+print "p.data.get('fakedata',None)  :", p.data.get('fakedata',None)
+
+if p.data.get('fakedata',None):
+    print "person has "
+else:
+    print ""
