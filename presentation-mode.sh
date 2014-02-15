fc28486c335dd34232c10b9fa32b6243993c67f3
Mike Putnam <mike@theputnams.net>
2014-02-15 12:04:08 -0800

    More options for large fonts in terminal.

--- presentation-mode.sh
+++ presentation-mode.sh
@@ -1,1 +1,4 @@
-xterm -ls -fn *-courier-bold-r-*-*-25-*-*-*-*-*-*
+#OpenBSD 5.3 stock fonts
+#xterm -ls -fn *-courier-bold-r-*-*-25-*-*-*-*-*-*
+#Ubuntu 12.04.3 stock fonts
+xterm -ls -fn "-*-courier 10 pitch-medium-r-*-*-25-*-*-*-*-*-*-*"
